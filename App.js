397d43db36cdcd478e225c03055d0b8dbb1550b8
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-04-05 20:42:48 +0530

    add: basic bounding | of App

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
+import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableNativeFeedback,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+  Platform,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={[styles.container]}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%",
+        }}
+      ></View>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
+
+// styles //
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? 30 : 0,
   },
 });
