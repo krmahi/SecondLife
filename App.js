7742440163bd30937d3bfd1a1068a731bcd11dc6
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-04-15 08:20:22 +0530

    add: ImagePicker and Native permissions

--- App.js
+++ App.js
@@ -1,6 +1,33 @@
-import React from "react";
-import LoginScreen from "./app/screens/LoginScreen";
+import React, { useEffect, useState } from "react";
+import * as ImagePicker from "expo-image-picker";
+import { Button, Image } from "react-native";
+
+import Screen from "./app/components/Screen";
 
 export default function App() {
-  return <LoginScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("You need to enable permission to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.canceled) setImageUri(result.assets[0].uri);
+    } catch (error) {
+      console.log("Error reading an image", error);
+    }
+  };
+  return (
+    <Screen>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Constants from "expo-constants";
 import { StyleSheet, SafeAreaView, StatusBar } from "react-native";
+import colors from "../config/colors";
 
 function Screen({ children, style }) {
   return (
