edf86a030dacd99841d7745859b0dd8c2fcd6ba3
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-04-28 13:00:20 +0530

    add: Loading Indicator for register and Login screen

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -2,16 +2,19 @@ import React from "react";
 import { View } from "react-native";
 import LottieView from "lottie-react-native";
 
-function ActivityIndicator({ visible }) {
+function ActivityIndicator({ visible, style }) {
   if (!visible) return null;
 
   return (
     <View>
       <LottieView
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
+        style={[
+          {
+            width: "100%",
+            height: "100%",
+          },
+          style,
+        ]}
         autoPlay
         loop
         source={require("../assets/animations/loader.json")}
