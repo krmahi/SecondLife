148e1633468ac16953647c63b3188bf2dafc9c88
Mahesh kumar <mahesh.kr.2277@gmail.com>
2024-05-07 20:39:45 +0530

    fix: expo-notifications

---
+++ app/hooks/useNotifications.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import * as Notifications from "expo-notifications";
+
+import expoPushTokenApi from "../api/expoPushTokens";
+import navigation from "../navigation/rootNavigation";
+
+export default useNotifications = (notificationListener) => {
+  useEffect(() => {
+    registerForPushNotifications();
+    if (notificationListener)
+      Notifications.addPushTokenListener(notificationListener);
+  }, []);
+
+  const registerForPushNotifications = async () => {
+    try {
+      const permission = await Notifications.getPermissionsAsync();
+      if (!permission.granted) return;
+      const token = (await Notifications.getExpoPushTokenAsync()).data;
+      expoPushTokenApi.register(token);
+    } catch (error) {
+      console.log("Error getting a push token", error);
+    }
+  };
+};
